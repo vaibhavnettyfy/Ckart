fe73575f0e4baad2c95eaf5349fbfc2a9a90c251
vaibhavNettyfy <vaibhavt.nettyfy@gmail.com>
2024-04-02 14:33:59 +0530

    Error Resolve

--- Providers.js
+++ Providers.js
@@ -3,6 +3,11 @@ import { Provider } from "react-redux";
 import store from "./src/app/store";
 
 
+if (!store) {
+    return;
+  }
+
+  
 export function Providers({children}) {
     return <Provider store={store}>{children}</Provider>
 }
