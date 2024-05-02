eef6a9d94ff555e98497396cbfb80b77eecf2ba4
vaibhavNettyfy <vaibhavt.nettyfy@gmail.com>
2024-05-02 12:12:25 +0530

    Search Bar Update

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,5 +1,4 @@
-"use client";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import React, { useState } from "react";
 import Image from "next/image";
 import { getAllProdutsApiHandler } from "@/Service/Product/Product.service";
@@ -9,6 +8,22 @@ const Search = () => {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
   const [productListData, setProductListData] = useState([]);
+  const [productName, setProductName] = useState('');
+  const [showProductList, setShowProductList] = useState(false);
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (searchRef.current && !searchRef.current.contains(event.target)) {
+      setShowProductList(false);
+    }
+  };
 
   useEffect(() => {
     getAllProductHandler();
@@ -20,7 +35,10 @@ const Search = () => {
 
   const searchHandler = async (value) => {
     getAllProductHandler(value);
+    setProductName(value);
+    setShowProductList(true);
   };
+
   const getAllProductHandler = async (value) => {
     try {
       setLoading(true);
@@ -38,13 +56,16 @@ const Search = () => {
       setLoading(false);
     }
   };
+
   return (
-    <div className="relative mx-auto text-gray-600 xl:w-[550px] md:w-[350px]">
+    <div ref={searchRef} className="relative mx-auto text-gray-600 xl:w-[550px] md:w-[350px]">
       <input
         className="bg-white w-full md:px-4 px-3 md:py-[10px] py-2 pr-10 rounded-md text-[#77878F] text-sm focus:outline-none"
         type="search"
         name="search"
         placeholder="Search for anything..."
+        autoComplete="off"
+        value={productName}
         onChange={(event) => searchHandler(event.target.value)}
       />
 
@@ -57,22 +78,29 @@ const Search = () => {
           className="md:w-5 w-4 md:h-5"
         />
       </button>
-      {productListData &&
-        productListData.length > 0 &&
-        productListData.map((response, index) => {
-          return (
-            <h3
-              key={index}
-              onClick={() =>
-                productHandler(response.category, response.subcategoryid)
-              }
-            >
-              {" "}
-              {response.productName}
-            </h3>
-          );
-        })}
-      <h>Testing</h>
+      {showProductList && (
+        <div className="bg-white absolute md:top-11 top-10 p-2 shadow-md max-h-[300px] overflow-auto w-full">
+          {productListData && productListData.length > 0 ? (
+            productListData.map((response, index) => {
+              return (
+                <div
+                  className="px-3 py-1 text-sm hover:bg-accent cursor-pointer"
+                  key={index}
+                  onClick={() => {
+                    productHandler(response.category, response.subcategoryid);
+                    setProductName(response?.productName);
+                    setShowProductList(false);
+                  }}
+                >
+                  {response.productName}
+                </div>
+              );
+            })
+          ) : (
+            <div className="px-3 py-1 text-center">No Data Found</div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
